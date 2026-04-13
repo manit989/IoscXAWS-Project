962005c46706f59be9fc7ba391eb6e0c1b567e80
Akshat Talwar <uniquedimension1@gmail.com>
2026-04-13 23:09:02 +0530

    adde old password route

--- app/routes/account.py
+++ app/routes/account.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import Depends, HTTPException, status, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.services.authHelper import User, get_current_user, get_user_by_id
+from app.services.authHelper import User, get_current_user, get_user_by_id, verify_password, ChangePasswordRequest
 from app.services.account_services import changeUserPassword, resetPasswordToDefault
 from app.core.database import get_db
 from app.model.models import DBUser, RoleEnum
@@ -24,11 +24,17 @@ async def whoAmI(
 
 @router.post("/change-password")
 async def change_password(
-    new_password: str,
+    request: ChangePasswordRequest,
     current_user: Annotated[User, Depends(get_current_user)],
     db: AsyncSession = Depends(get_db)
 ):
-    await changeUserPassword(db, current_user.id, new_password)
+    db_user = await get_user_by_id(db, current_user.id)
+    if not await verify_password(request.old_password, db_user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Current password is incorrect"
+        )
+    await changeUserPassword(db, current_user.id, request.new_password)
     return {"message": "password change successful"}
 
 
